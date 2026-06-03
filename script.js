41fa71159b2e9aa0ccac5d56411f7932494ea1e7
shivanshu <devshivansh11@gmail.com>
2026-06-03 15:08:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+ function updateClock() {
+
+            let now = new Date();
+
+            let hours = now.getHours();
+            let minutes = now.getMinutes();
+            let seconds = now.getSeconds();
+
+            let ampm = "AM";
+
+            if(hours >= 12){
+                ampm = "PM";
+            }
+
+            if(hours > 12){
+                hours = hours - 12;
+            }
+
+            if(hours == 0){
+                hours = 12;
+            }
+
+            if(hours < 10){
+                hours = "0" + hours;
+            }
+
+            if(minutes < 10){
+                minutes = "0" + minutes;
+            }
+
+            if(seconds < 10){
+                seconds = "0" + seconds;
+            }
+
+            document.getElementById("time").innerHTML =
+                hours + ":" + minutes + ":" + seconds + " " + ampm;
+        }
+
+        setInterval(updateClock, 1000);
+        updateClock();
